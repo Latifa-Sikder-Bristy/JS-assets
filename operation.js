12f78b88fa67a5f9b3ae1e91629501272101efa5
Latifa-Sikder-Bristy <latifasikderbristy@gmail.com>
2023-07-29 15:49:14 +0600

    Added Mathematical operation shorthand

---
+++ operation.js
@@ -0,0 +1,76 @@
+var onionPrice = 20;
+var eggPrice = 10;
+
+
+// addition 
+var totalPrice = onionPrice + eggPrice;
+console.log(totalPrice);
+// sub 
+var priceDifference = onionPrice - eggPrice;
+console.log(priceDifference);
+
+// mul
+var orangePRice = 20;
+var qty = 7;
+var totalCost = orangePRice * qty;
+console.log(totalCost);
+
+// div 
+var money = 500;
+var player = 10;
+var eachPlayer = money / player;
+console.log(eachPlayer);
+
+
+// console.log(onionPrice);
+// console.log(eggPrice);
+// console.log(onionPrice + eggPrice);
+
+// :::advance::::
+// var declear
+var toamtoPrice = 25;
+// var value update
+toamtoPrice = 30;
+
+// add something to the previous value
+var price1 = 32;
+var price2 = 10;
+
+// set a new value
+// var price1 = 40;
+
+// add some value to the existing value
+price1 = price1 + 10;
+console.log(price1);
+
+
+// addition to the same var shorthand
+// += means adding some value to the existing value
+price1 += 10;
+console.log(price1);
+
+// minus to the same var shorthand
+// -= means adding some value to the existing value
+price1 = price1 - 7;
+price1 -= 7;
+console.log(price1);
+
+// *=
+price1 *= 7;
+console.log(price1);
+
+// /=
+price1 /= 7;
+console.log(price1);
+
+var age = 15;
+age = age + 1;
+// ++ means adding 1 to the existing
+age++
+console.log(age);
+
+var love = 100;
+love = love -1;
+//--means minus 1 to the existing one  
+love--;
+console.log(love);
--- variable.js
+++ variable.js
@@ -1,7 +1,16 @@
 // vary + able = variable
+// numeric
 var price = 99;
+var count = 12;
+
+// string
 var product = 'egg';
 var company = 'chaldal';
-var count = 12;
+var location = "anodor killay bandorban";
+
+// boolean
+var serious = true;
+var isFullMarks = false;
+var isString = false;
 
 console.log(company+product+product);
