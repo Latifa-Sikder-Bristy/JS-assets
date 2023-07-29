7340f6fb860a982ab751ea737ec766152b4f0f0f
Latifa-Sikder-Bristy <latifasikderbristy@gmail.com>
2023-07-29 15:55:45 +0600

    concatenation.js

---
+++ concatenation.js
@@ -0,0 +1,10 @@
+var price1 = 31;
+var price2 = 22;
+var totalPrice = price1 + price2;
+// console.log(totalPrice);
+
+var fastNamw = 'Jennie';
+var lastName = 'Kim';
+// create string concatenation 
+var fullName = fastNamw + ' '+ lastName;
+console.log(fullName);
