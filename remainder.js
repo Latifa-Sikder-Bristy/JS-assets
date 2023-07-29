e6745dd43344cdeaaff9ccc9cd20277f9fd18e8c
Latifa-Sikder-Bristy <latifasikderbristy@gmail.com>
2023-07-29 16:21:02 +0600

    modulas remainder

---
+++ remainder.js
@@ -0,0 +1,9 @@
+var mango = 7;
+var person = 3;
+//means remainder value
+var divided = mango / person;
+console.log(divided);
+
+//means remainder/ moduler operator value
+var remainder = mango % person;
+console.log(remainder);
