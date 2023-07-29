1ffe2617de794c44f7a899523f253429d998eb02
Latifa-Sikder-Bristy <latifasikderbristy@gmail.com>
2023-07-29 16:06:11 +0600

    conversion

---
+++ conversion.js
@@ -0,0 +1,25 @@
+var price1 = '31';
+var price2 = '10';
+var totalPrice = price1 + price2;
+// console.log(totalPrice);
+
+var price1 = '31';
+var price2 = 10;
+var totalPrice = price1 + price2;
+console.log(totalPrice);
+
+// convert string to number 
+console.log(price1);
+priceCon = parseInt(price1)
+// convert string to number 
+console.log(priceCon);
+console.log(price2);
+
+
+//
+var gpa = '3.41';
+console.log(gpa);
+var gpaNum = parseInt(gpa);
+console.log(gpaNum);
+var gpaNum = parseFloat(gpa);
+console.log(gpaNum);
