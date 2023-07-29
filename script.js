242fd4c329272bc6ccdb8cc3219580b6b4f13c81
Latifa-Sikder-Bristy <latifasikderbristy@gmail.com>
2023-07-29 14:32:38 +0600

    Math list

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 console.log(8)  //Number
 console.log(3365397)
+
+/* 
+1. number ==> 55, -5, 78.6
+2. integer : even, odd, 3, 5
+3. float:  4.0, 6.7
+4. NaN (Not a Number): Infinity (-Infinity), undefined
+5. operation: add, sub, muil, div, reminder
+6. percent
+7. avg
+8. comparison: <, >, ==, !=, <=, >=
+9. Oikik: 2 kilo apple and 5 kilo grape total weight
+10. 45, 75, 2342, 6 : max, min, avg, sum
+11. series: sum of series, odd, even 
+:::Extra:::
+1. Prime
+2. Fibonacci
+3. Permutaion ---- "ABC" is 3! = 6, 
+                    6 ways to arrange the letters: ABC, ACB, BAC, BCA, CAB, and CBA.
+:::::::ALWAYS mindset::::::::
+    :::::::GOOGLE::::::::
+:::::Positive mindest:::::
+- GCD, LCM
+- area of trianngle
+ */
