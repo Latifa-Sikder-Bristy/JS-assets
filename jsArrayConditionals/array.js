5e5adedc0e35b18180435276ed79eed4201a323e
Latifa-Sikder-Bristy <latifasikderbristy@gmail.com>
2023-07-30 14:21:16 +0600

    array index

---
+++ jsArrayConditionals/array.js
@@ -0,0 +1,13 @@
+//number
+var friendsAge = [11, 21, 45, 17, 14, 105, 6];
+var picnicFree = [5000, 2000, 4000, 150];
+var nayikas = ["mahi", "opu", "sabnor", "sabana"];
+var vawels = ["a", "e", "i", "o", "u"];
+
+console.log(picnicFree);
+
+//array length
+console.log(picnicFree.length);
+
+// index
+// 0 1 2 3 4 5 6 7 
---
+++ jsArrayConditionals/index.js
@@ -0,0 +1,20 @@
+var numbers = [11, 21, 45, 17, 14, 105, 6];
+
+//get the element by index
+console.log(numbers);
+
+//get the nth element by index
+// console.log(numbers[2]);
+
+//get the nth element by index
+var index = numbers[2];
+console.log(index);
+
+//save value on index position
+numbers[1] = 77
+console.log(numbers[1]);
+
+//give me the index of element
+var position = numbers.indexOf(17);
+console.log(position);
+//outut -1 means this element not int the array
