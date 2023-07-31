3628595be9166310be6fc31a9c8460fff2eabd66
Latifa-Sikder-Bristy <latifasikderbristy@gmail.com>
2023-07-31 14:29:44 +0600

    arary-loop

---
+++ loop/array-loop.js
@@ -0,0 +1,21 @@
+/* for(var i =0; i < 10; i++){
+    console.log(i);
+} */
+
+//target: display every elements of an array
+/* var numbers = [34, 54, 54, 77, 32, 34, 65, 98];
+
+for (var i = 0; i < numbers.length; i++){
+    //number the single number and 
+    //numbers the array of number
+    var number = numbers[i];
+    // like array[index]
+    console.log(number);
+} */
+
+var items = ['book','laptop', 'monitor', 'headphone','bottle']
+
+for (var i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item);
+}
---
+++ loop/for.js
@@ -0,0 +1,30 @@
+/* var roastGiven = 0;
+
+while (roastGiven <7) {
+    console.log('Roast den, please!!');
+    roastGiven++;
+    //without this line get infinit
+}
+ */
+
+/* for(var roastGiven = 0; roastGiven < 7; roastGiven++){
+    console.log('Roast den, pleaseeeeee !!');
+} */
+
+//simple version of for loop
+/* console.log('Simple for loop');
+for(var i = 0; i < 7; i++){
+    console.log(i);
+} */
+
+//even
+/* for(var i = 1; i <= 10; i++){
+    console.log(i);
+} */
+
+//even
+//i = i + 2;
+//i += 2;
+for(var i = 2; i <= 10; i+=2){
+    console.log(i);
+}
