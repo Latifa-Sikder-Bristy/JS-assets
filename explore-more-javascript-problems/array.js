2fa3dd5908b31cdfcdcb666834f5fe328feb63f9
Latifa-Sikder-Bristy <latifasikderbristy@gmail.com>
2023-08-08 18:38:55 +0600

    explore more js problems

---
+++ explore-more-javascript-problems/array.js
@@ -0,0 +1,7 @@
+const country = 'Bangladesh';
+const age = 52;
+const isIndependent = true;
+const student = {id: 432, class:11, name: 'Agun'};
+const friends = [13, 12, 15, 16, 17, 21, 18]
+
+console.log(typeof country);
