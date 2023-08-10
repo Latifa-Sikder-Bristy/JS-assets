554dae22a4662e53a692e1391f8de3a66f02e2b2
Latifa-Sikder-Bristy <latifasikderbristy@gmail.com>
2023-08-10 19:38:24 +0600

    findAddress done

--- problemSolving/Function/findAddress.js
+++ problemSolving/Function/findAddress.js
@@ -1,29 +1,27 @@
-function findAddress(obj) {
+/* function findAddress(obj) {
     const keys = Object.keys(obj);
     const values = Object.values(obj);
 
-    const joinObj = values.join(',');
-/* 
-    if(values.length < 3){
-        const output = values.splice(1, 3, "__");
-    } */
-
-
-/*     if(keys.length < 3 || values.length < 3 ){
-        const joinObj = obj.splice(1, 3, "__");
-        console.log(joinObj);
-    } */
-    return joinObj;
+    if(keys.length == 3){
+        var joinObj = values.join(',');
+        return joinObj;
+    }else{
+        var joinObj = values.join(',');
+        var joinObj = values.join('__');
+    }
+    console.log(keys.length);
 }
+
 
 
 
 const sample = {
     street:10,
-    age: 30,
+    // age: 30,
     society: 'Earth Perfect'
 };
 console.log(findAddress(sample));
+ */
 
 
 
@@ -36,3 +34,18 @@ console.log(findAddress(sample));
 
 const newSong = lines.join('.')
 console.log(newSong) */
+
+
+function findAddress(obj) {
+    const street    = obj.street ? obj.street : '__';
+    const house       = obj.house ? obj.house : '__';
+    const society   = obj.society ? obj.society : '__';
+    return street +','+ house +','+ society ;
+}
+
+const sample = {
+    street:10,
+    house: '15A',
+    society: 'Earth Perfect'
+};
+console.log(findAddress(sample));
