f24c3c9fee41f3de0d0e1f42ae0a45ca1b2fd0a0
Latifa-Sikder-Bristy <latifasikderbristy@gmail.com>
2023-08-10 09:24:09 +0600

    a4 matchFinder final

--- problemSolving/Function/a4.js
+++ problemSolving/Function/a4.js
@@ -5,3 +5,11 @@ function cubeNumber(number){
     const result = Math.abs(Math.pow(number, 3));
     return result;
 }
+
+function matchFinder(string1, string2) {
+    if (typeof string1 !== "string" || typeof string2 !== "string") {
+        return 'Please enter string';
+    }
+    const indexOf = string1.indexOf(string2);
+    return indexOf !== -1;
+}
--- problemSolving/Function/matchFinder.js
+++ problemSolving/Function/matchFinder.js
@@ -1,6 +1,6 @@
 function matchFinder(string1, string2) {
     if (typeof string1 !== "string" || typeof string2 !== "string") {
-        return 'Please enter a string';
+        return 'Please enter string';
     }
     const indexOf = string1.indexOf(string2);
     return indexOf !== -1;
