6ce7c0e0f932ab24982ff94014bdd24352cb6e09
Latifa-Sikder-Bristy <latifasikderbristy@gmail.com>
2023-08-20 12:36:29 +0600

    Different ways to use addEventListener on a button

---
+++ event/js/events.js
@@ -0,0 +1,13 @@
+console.log('saperate js');
+
+
+//option 2: onclick function
+//important : we will use this most of the time
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+//option 3: onclick function
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
